754265c4976fb5143a688f30d75281d3f092808c
germania (Ben Clarke) <me@benclarke.ca>
2012-06-25 16:00:24 -0400

    Going for broke with a 3-state toggle

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/toggles/SoundVibSilentToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/toggles/SoundVibSilentToggle.java
@@ -1,23 +1,83 @@
 package com.android.systemui.statusbar.policy.toggles;
 
+import com.android.systemui.R;
+
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
 
 public class SoundVibSilentToggle extends Toggle {
 
 	public SoundVibSilentToggle(Context c) {
 		super(c);
+		this.availableStates = 3;
+		
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(AudioManager.RINGER_MODE_CHANGED_ACTION);
+        c.registerReceiver(new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                updateState();
+            }
+            
+        }, filter);
+		
 	}
 
 	@Override
 	protected boolean updateInternalToggleState() {
-		// TODO Auto-generated method stub
-		return false;
+		
+		AudioManager am = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        int mode = am.getRingerMode();
+        switch(mode) {
+	        case AudioManager.RINGER_MODE_NORMAL: 
+	        	this.state = 0;
+	        break;
+	        case AudioManager.RINGER_MODE_VIBRATE: 
+	        	this.state = 1;
+	        break;
+	        case AudioManager.RINGER_MODE_SILENT: 
+	        	this.state = 2; 
+	        break;
+        }
+        
+        this.updateIcon();
+        
+        return state == 0;
+		
 	}
 
 	@Override
 	protected void onCheckChanged() {
-		// TODO Auto-generated method stub
 
+		AudioManager am = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+		
+		switch(this.state) {
+			case 0: am.setRingerMode(AudioManager.RINGER_MODE_NORMAL); break;
+			case 1: am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE); break;
+			case 2: am.setRingerMode(AudioManager.RINGER_MODE_SILENT); break;
+		}
+		
+		this.updateIcon();
+
+	}
+	
+	private void updateIcon() {
+		switch(this.state) {
+	        case 0: 
+	        	setIcon(R.drawable.toggle_silence_off);
+	        break;
+	        case 1: 
+	        	setIcon(R.drawable.toggle_vibrate);
+	        break;
+	        case 2: 
+	        	this.state = 2; 
+	        	setIcon(R.drawable.toggle_silence);
+	        break;
+	    }
 	}
 
 	@Override

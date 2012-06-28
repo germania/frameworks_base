1f4c6c174d624349aaaae2a1bde76ef2babd7e01
germania (Ben Clarke) <me@benclarke.ca>
2012-06-28 15:30:45 -0400

    Layout updates

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/toggles/Toggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/toggles/Toggle.java
@@ -181,7 +181,7 @@ public abstract class Toggle implements OnClickListener {
     }
     
     private LinearLayout.LayoutParams getParams(float weight) {
-    	return new LinearLayout.LayoutParams(0, 30, weight);
+    	return new LinearLayout.LayoutParams(0, 32, weight);
     }
 
     /**
